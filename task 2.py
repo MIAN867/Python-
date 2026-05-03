69670be3b52bda3bce4b4db27f85e41202577dc7
Mudassar Sajid <ms2624984@gmail.com>
2026-05-03 09:23:56 +0500

    task 2

---
+++ task 2.py
@@ -0,0 +1,17 @@
+
+while True:
+    message= input("you: ")
+    if message == "hello":
+        print(" Bot: hi There!")
+    elif message == "bye":
+    
+        print("Bot: Good bye")
+        break 
+    elif message== "how are you?":
+        print("i am fine!")
+    elif message=="what is your name":
+        print("i am a pyBot!")
+    elif message=="age":
+        print("i am a bot _ no age!")       
+    else:
+        print("Bot: i don't understand!")
